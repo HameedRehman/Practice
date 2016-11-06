26cb1c92f3d82403d28e73da2f2d443addcadf41
HameedRehman <hameedrehmanbwn94@yahoo.com>
2016-11-06 21:41:57 +0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<div class="footer">
+    <h5 align="center"> <br>
+        <a href="<?php echo base_url().'home/our_mission'?>"> Our mission | </a>
+        <a href="<?php echo base_url().'home/contact_us'?>"> Contact us | </a>
+        <a href="<?php echo base_url().'home/about_us'?>"> About us </a>
+    </h5>
+    <h5 align="center"><a href="<?php echo base_url().'admin_login'?>"> Admin Login </a></h5>
+    <h5 align="center">
+        <p> &copy; Copyright 2014 - 2016, British Columbia Institute of Technology.
+            Last updated on Mar 21, 2016.
+        </p>
+    </h5>
+</div>
+</div>
+</body>
+</html>
