7b9a5d8ad9c297ac8c719abc8f7a76fa94a55f48
HameedRehman <hameedrehmanbwn94@yahoo.com>
2016-11-06 22:03:40 +0500

    Create notification_controller.php

---
+++ notification_controller.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of login
+ *
+ * @author Mohammad Waqas Amjad
+ */
+class Admin_notification extends CI_Controller{
+    //put your code here
+    
+   
+    
+     public function index() {
+     	 
+         $this->load->view('admin/Admin_notification');
+      }
+   
+   
+}
