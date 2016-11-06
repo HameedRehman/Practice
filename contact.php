ebec9dba491439cb9f2836173e439ea39fe0df87
HameedRehman <hameedrehmanbwn94@yahoo.com>
2016-11-06 21:46:00 +0500

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php include_once('header.php'); ?> 
+  <div>
+      <legend><h3 align="center"> Contact Us </h3> </legend>
+      <div class="min-height">
+        
+    </div>       
+</div>
+<?php include_once('footer.php');
+
