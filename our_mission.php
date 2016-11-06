f056d0b89331dfe013a7fb0bfbfd6d9e01283313
HameedRehman <hameedrehmanbwn94@yahoo.com>
2016-11-06 21:49:05 +0500

    Create our_mission.php

---
+++ our_mission.php
@@ -0,0 +1,18 @@
+<?php include_once('header.php'); ?> 
+  <div>
+      <legend><h3 align="center"> Wellcome to E-Buy.com </h3> </legend>
+    <div id="img-body">
+        <img src="<?php echo base_url().'images/sec.gif'?>" height="400px" width="450px" class="img-responsive img-rounded" alt="image not found">
+    </div>
+    <div>
+        
+        <center> <h2> <b> Our Mission </b></h2> </center>
+          <p>   The most well-known dummy text is the 'Lorem Ipsum', which is said to have originated in the 16th century. Lorem Ipsum is composed in a pseudo-Latin language which more or less corresponds to 'proper' Latin. It contains a series of real Latin words. This ancient dummy text is also incomprehensible, but it imitates the rhythm of most European languages in Latin script. The advantage of its Latin origin and the relative meaninglessness of Lorum Ipsum is that the text does not attract attention to itself or distract the viewer's attention from the layout.
+
+    One disadvantage of Lorum Ipsum is that in Latin certain letters appear more frequently than others - which creates a distinct visual impression. Moreover, in Latin only words at the beginning of sentences are capitalized; this means that Lorem Ipsum cannot accurately represent, for example, German, in which all nouns are capitalized. Thus, Lorem Ipsum has only limited suitability as a visual filler for German texts. If the fill text is intended to illustrate the characteristics of different typefaces, it sometimes makes sense to select texts containing the various letters and symbols specific to the output language.
+        </p>
+    </div>       
+</div>
+<?php include_once('footer.php');
+
+
